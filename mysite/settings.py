4a5344d3bd25cad4f60226cb4cd057e2dceb3f08
Yerassylbek <maxyerassylbek@gmail.com>
2023-08-25 20:04:16 +0600

    zakoncili 2nd part of book

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'thundersamsa@gmail.com'
+EMAIL_HOST_PASSWORD = 'yrebdugxygubetdn'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
