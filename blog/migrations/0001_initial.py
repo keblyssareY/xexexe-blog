bfae9479a20fde1f72f0e009f37e581db26d7f3e
Yerassylbek <maxyerassylbek@gmail.com>
2023-08-25 15:55:42 +0600

    second commit

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -20,7 +20,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=250)),
-                ('slug', models.SlugField(max_length=250, unique_for_date='publish')),
+                ('slug', models.SlugField(max_length=250)),
                 ('body', models.TextField()),
                 ('publish', models.DateTimeField(default=django.utils.timezone.now)),
                 ('created', models.DateTimeField(auto_now_add=True)),
